2602687175dabf4a5ef08f3d173b43ce8dfa3df0
Stephen Finney <stephen@codeforamerica.org>
2013-10-26 19:23:25 -0400

    Remove comments and add commit()

--- init_db.py
+++ init_db.py
@@ -6,23 +6,8 @@ mydb = sqlite3.connect('log.sqlite')
 cursor = mydb.cursor()
 
 # Use this code to remove the table(s) for the database
-#cursor.execute("DROP TABLE ICMP;")
-#cursor.execute("DROP TABLE TCP;")
-#cursor.execute("DROP TABLE UDP;")
+#cursor.execute("DROP TABLE Log;")
 
-#cursor.execute('CREATE TABLE ICMP' + \
-#					'(DATE string, PROTOCOL string, CONNECTION varchar(1), ' + \
-#					'SRCIP string, DSTIP string, INFO string, COMMENT string);')
-
-#cursor.execute('CREATE TABLE TCP' + \
-#					'(DATE string, PROTOCOL string, CONNECTION varchar(1), ' + \
-#					'SRCIP string, SRCPORT string, DSTIP string, ' + \
-#					'DSTPORT string, INFO string, COMMENT string);')
-
-#cursor.execute('CREATE TABLE UDP' + \
-#					'(DATE string, PROTOCOL string, CONNECTION varchar(1), ' + \
-#					'SRCIP string, SRCPORT string, DSTIP string, ' + \
-#					'DSTPORT string, INFO string, COMMENT string);')
 					
 cursor.execute('Create Table Log' + \
 			   '(Date string, Time string, Protocol string,'+\
@@ -30,5 +15,6 @@ cursor.execute('Create Table Log' + \
 			   'Dest_IP string, Dest_Port string, Info string,'+\
 			   'Environment string);')
 
+mydb.commit()
 cursor.close()
 print("Database initialized.")
