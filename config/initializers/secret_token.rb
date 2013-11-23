600bce4844de1deedfe44466a1d45f54ff782a92
ssfinney <stephen@codeforamerica.org>
2013-11-23 12:35:26 -0500

    Init Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HoneypotAnalyzer::Application.config.secret_key_base = 'b72bb9ef306040e5d565927edd897993cdf321846b542cc2369fbf27e55ebe86b3c9c7826fc8efd8150b9ab91041fad9b6009eef891a14cceb58ae08ce092207'
