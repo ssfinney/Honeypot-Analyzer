5b4d41760b198e43aaa3e4aea7e6296e419a8b13
ssfinney <stephen@codeforamerica.org>
2013-11-30 15:48:13 -0500

    Fill in entry and log controllers

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,3 +1,42 @@
 class EntriesController < ApplicationController
 
+  def index
+    @entries = Entry.all
+  end
+
+  def new
+    @log = Log.find(params[:log_id])
+    @entry = Entry.new
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @entry }
+    end
+  end
+
+  def create
+    @log = Log.find(params[:log_id])
+    @entry = @log.entries.new(params[:entry])
+
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to @entry, notice: 'Log entry was successfully created.' }
+        format.json { render json: @entry, status: :created }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @entry = Entry.find([params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @entry }
+    end
+  end
+
 end
+
--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,3 +1,22 @@
 class LogsController < ApplicationController
+	
+  def index
+    @logs = Log.all
+  end
+
+  def new
+    @log = Log.new
+  end
+
+  def create
+    @log = Log.new(params[:log])
+    @log.save
+    respond_with @log
+  end
+
+  def show
+    @log = Log.find([params[:id])
+  end
 
 end
+
